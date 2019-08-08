cb66127600f2daf49ffb9eefcfe8b1e895b1a9d8
Rocky Li <aperocky@gmail.com>
2019-08-07 23:27:14 -0400

    Add more stuff to server and wolf

--- server.py
+++ server.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import render_template, request, session, redirect, jsonify
 from flask_cors import CORS
 import wolf
+import uuid
 
 app = Flask(__name__, template_folder='templates', static_folder='assets')
+
+GAME_START_FLAG = False
+wolfgame = wolf.WolfGame() # Wolf placeholder game
 
 # Create and expose the game instance
 def create_game(config_dict):
@@ -13,17 +17,21 @@ def create_game(config_dict):
 # This is a host call
 @app.route("/init")
 def init():
-    pass
+    initargs = request.form.to_dict()
+    create_game(initargs)
 
 # this is a host call
 @app.route("/start")
 def start():
-    pass
+    if wolfgame.startable:
+        return True
+    return False
 
 # This is a client call
 @app.route("/seat")
 def seat():
     seat_num = request.args.get("SEAT")
+    
     pass
 
 # This is a client call from wolf character
--- wolf.py
+++ wolf.py
@@ -13,6 +13,7 @@ class WolfGame:
         self.WOLF_FLAG = False
         self.PREDICT_FLAG = False
         self.CURE_FLAG = False
+        self.killed = -1 # Nobody
 
         # Randomly assign characters.
         self.assign_role()
@@ -23,6 +24,31 @@ class WolfGame:
             ["PERCEIVAL"]
         random.shuffle(roles)
         self.roles = roles
+        self.players = [[role, ""] for role in self.roles]
 
-    def 
+    def take_seat(self, uid, index):
+        self.players[index][1] = uid
+
+    def startable(self):
+        if all(e[1] for e in self.players):
+            self.START_FLAG = True
+            return True
+        return False
+
+    def set_kill(self, index):
+        self.killed = index
+        self.WOLF_FLAG = True
+
+    def predict(self, index):
+        self.PREDICT_FLAG = True
+        return self.roles[index]
+
+    def get_kill(self):
+        return self.killed
         
+    def cure(self, action):
+        if action:
+            if not self.killed < 0:
+                self.killed = -1
+        self.CURE_FLAG = True   
+            
