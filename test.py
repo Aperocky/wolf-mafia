350c7db8373c4ba33348dfac77c69b8ddf1c67eb
Rocky Li <aperocky@gmail.com>
2019-08-10 10:15:56 -0400

    Completed test and fix bug

--- test.py
+++ test.py
@@ -14,8 +14,28 @@ assert(wolfgame.total_player == 12)
 
 # Create 12 Users and seat them
 users_uuid = [str(uuid.uuid4()) for i in range(12)]
-[wolfgame.take_seat(uid, i) for i, uid in enumerate(users_uuid)]
+roles = [wolfgame.take_seat(uid, i) for i, uid in enumerate(users_uuid)]
+print(roles)
 wolfgame.start_game()
-
 time.sleep(20)
+
+# Get a wolf to kill a random person:
+wolves = [(i, e[0]) for i, e in enumerate(roles) if e[1] == "WOLF"]
+print(wolves)
+
+status = wolfgame.set_kill(0)
+assert(status)
+time.sleep(30)
+
+role = wolfgame.predict(wolves[1][0])
+assert(role == "WOLF")
+time.sleep(30)
+
+killed = wolfgame.get_kill()
+assert(killed == 0)
+time.sleep(5)
+wolfgame.cure(False)
+
+time.sleep(10)
+wolfgame.result()
 
--- wolf_cli.py
+++ wolf_cli.py
@@ -1,5 +1,5 @@
 """
-This script is to handle events from wolf, create audible events with threading so as to be non-blocking. 
+This script is to handle events from wolf, create audible events with threading so as to be non-blocking.
 """
 
 import logging
@@ -16,6 +16,7 @@ stdout_handler.setLevel(logging.DEBUG)
 logger.addHandler(stdout_handler)
 
 SOUND_ASSET_DIR = "assets/sound/"
+GLOBAL_THREAD_COUNT = 0
 
 def play_record(filename):
     import sounddevice as sd
@@ -31,10 +32,23 @@ def thread_run(target_func):
         thread = threading.Thread(target=target_func, args=tuple(args))
         thread.start()
     return wrapper
-    
+
+def single_threaded(target_func):
+    def wrapper(*args):
+        global GLOBAL_THREAD_COUNT
+        while GLOBAL_THREAD_COUNT > 0:
+            print("blocking thread due to existing running thread")
+            time.sleep(5)
+        GLOBAL_THREAD_COUNT = 1
+        target_func(*args)
+        print("resetting global threadcount")
+        GLOBAL_THREAD_COUNT = 0
+    return wrapper
+
 @thread_run
+@single_threaded
 def wolf_start():
-    time.sleep(5)
+    time.sleep(2)
     play_record(SOUND_ASSET_DIR + "night_fall_close_eye")
     time.sleep(2)
     play_record(SOUND_ASSET_DIR + "wolf_open_eye")
@@ -42,24 +56,27 @@ def wolf_start():
     play_record(SOUND_ASSET_DIR + "wolf_please_kill")
 
 @thread_run
+@single_threaded
 def perceival_start():
     time.sleep(2)
     play_record(SOUND_ASSET_DIR + "wolf_already")
-    time.sleep(5)
+    time.sleep(3)
     play_record(SOUND_ASSET_DIR + "perceival_open")
     time.sleep(2)
     play_record(SOUND_ASSET_DIR + "perceival_decide")
 
 @thread_run
+@single_threaded
 def witch_start():
     time.sleep(2)
     play_record(SOUND_ASSET_DIR + "perceival_close")
-    time.sleep(5)
+    time.sleep(3)
     play_record(SOUND_ASSET_DIR + "witch_open")
     time.sleep(2)
     play_record(SOUND_ASSET_DIR + "witch_save_or_not")
-    
+
 @thread_run
+@single_threaded
 def end_night():
     time.sleep(2)
     play_record(SOUND_ASSET_DIR + "witch_close")
@@ -67,13 +84,14 @@ def end_night():
     play_record(SOUND_ASSET_DIR + "dawn")
 
 @thread_run
+@single_threaded
 def announce_result(index):
     time.sleep(2)
     if index < 0:
         play_record(SOUND_ASSET_DIR + "end_result_peace")
     else:
         play_record(SOUND_ASSET_DIR + "end_result_mort_first")
-        subprocess.call(["say", index])
+        subprocess.call(["say", str(index)])
         time.sleep(0.3)
         play_record(SOUND_ASSET_DIR + "end_result_mort_second")
 
